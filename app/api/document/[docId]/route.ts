901822b50c33f1ec84525d40544e6e7214b16f85
Aryan shinde <shindearyan179@gmail.com>
2024-07-12 15:12:13 +0530

    Fixed GET error on the Document/[id]

--- app/api/document/[docId]/route.ts
+++ app/api/document/[docId]/route.ts
@@ -2,30 +2,6 @@ import { db } from "@/app/utils/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// TODO: Get Document using the id
-export async function GET({ params }: { params: { docId: string } }) {
-  try {
-    const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse("User Not Authenticated", { status: 401 });
-    }
-
-    // const { title, description } = await req.json();
-
-    const getDocument = await db.document.findUnique({
-      where: {
-        id: params.docId,
-        userId: userId,
-      },
-    });
-
-    return new NextResponse(JSON.stringify(getDocument), { status: 200 });
-  } catch (error) {
-    return new NextResponse("GET Error", { status: 500 });
-  }
-}
-
 export async function PUT(
   req: Request,
   { params }: { params: { docId: string } }
